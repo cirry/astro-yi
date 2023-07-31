f80ccc1ac0530b3dbbcf0d00b8243c3de622f035
cirry <812852553@qq.com>
2023-07-31 16:43:29 +0800

    fix: 修复rss

--- src/pages/rss.xml.js
+++ src/pages/rss.xml.js
@@ -1,26 +1,27 @@
 import rss from '@astrojs/rss';
-import { site as SITE } from "$/config.yml";
-
-const allPosts = import.meta.glob('./blog/**/*.md', { eager: true })
-const sortedPosts = Object.values(allPosts).sort((a, b) => new Date(b.date) - new Date(a.date));
+import { site as SITE } from "../config.yml";
+import { getCollectionByName } from 'src/utils/getCollectionByName';
+import { sortPostsByDate } from "src/utils/sortPostsByDate";
+let allPosts = await getCollectionByName('blog')
+let sortedPosts = sortPostsByDate(allPosts)
 
 export const get = () => rss({
-    // `<title>` field in output xml
-    title: `${SITE.name} | Blog`,
-    // `<description>` field in output xml
-    description: SITE.description,
-    // base URL for RSS <item> links
-    // SITE will use "site" from your project's astro.config.
-    site: import.meta.env.SITE,
-    // list of `<item>`s in output xml
-    // simple example: generate items for every md file in /src/pages
-    // see "Generating items" section for required frontmatter and advanced use cases
-    items: sortedPosts.map(item => ({
-        title: item.frontmatter.title,
-        description: item.frontmatter.description,
-        link: item.url,
-        pubDate: item.frontmatter.date,
-    })),
-    // (optional) inject custom xml
-    customData: `<language>en-us</language>`,
+  // `<title>` field in output xml
+  title: `${SITE.title}`,
+  // `<description>` field in output xml
+  description: SITE.description,
+  // base URL for RSS <item> links
+  // SITE will use "site" from your project's astro.config.
+  site: import.meta.env.SITE,
+  // list of `<item>`s in output xml
+  // simple example: generate items for every md file in /src/pages
+  // see "Generating items" section for required frontmatter and advanced use cases
+  items: sortedPosts.map(item => ({
+    title: item.data.title,
+    description: item.data.description,
+    link: item.url,
+    pubDate: item.data.date,
+  })),
+  // (optional) inject custom xml
+  customData: `<language>en-us</language>`,
 });
