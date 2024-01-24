c384455b1788f6cdf35054b7ff44c446fc06c1f7
cirry <812852553@qq.com>
2024-01-24 18:43:54 +0800

    fix：修复没有文章报错

--- src/utils/getCollectionByName.ts
+++ src/utils/getCollectionByName.ts
@@ -2,8 +2,11 @@ import {getCollection} from "astro:content";
 
 export const getCollectionByName = async (name: string) => {
   let posts = await getCollection(name)
-  return posts.filter(post => !post.data.draft).map(post => {
-    return post
-  })
-
+  if (posts && posts.length > 0 ) {
+    return posts.filter(post => !post.data.draft).map(post => {
+      return post
+    })
+  }else {
+    return []
+  }
 }
