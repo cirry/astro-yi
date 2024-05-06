539058b492ed0315dd5cc7a3525f728706ffe283
EmVee <emvee381@gmail.com>
2024-05-06 12:50:04 +0200

    - Updated date format selection functionality. - Added formatting function, including a parameter to support multiple types of formatting strings based on localization from i18n. - Added Czech language primarily for testing purposes.

--- src/utils/formatDate.ts
+++ src/utils/formatDate.ts
@@ -1,9 +1,17 @@
 import dayjs from "dayjs";
+import advancedFormat from 'dayjs/plugin/advancedFormat'
 import utc from "dayjs/plugin/utc";
+import {t} from '../i18n/utils';
+import {config} from '../consts';
+
+dayjs.locale(config.lang);
+dayjs.extend(advancedFormat)
 dayjs.extend(utc);
-export function formatDate(date) {
+
+export function formatDate(date,dateType='post.dateFormat') {
   if (date) {
-    return dayjs(date).utc().format("YYYY-MM-DD")
+    const dateFormat = t(dateType) || "YYYY-MM-DD";
+    return dayjs(date).utc().format(dateFormat);
   } else {
     return ''
   }
