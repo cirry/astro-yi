01a87f76af4c8c050898eb73b4a895afc6875a61
cirry <>
2024-10-22 11:54:21 +0800

    fix utils.ts

--- src/i18n/utils.ts
+++ src/i18n/utils.ts
@@ -4,8 +4,8 @@ import {cs} from './cs'
 import {zhHant} from './zhHant'
 import {config} from "../consts";
 const ui = {
-  en, 'zh-cn': zhCn, 'zh-Hant': zhHant,  cs
-
+  en, 'zh-cn': zhCn, 'zh-Hant': zhHant, cs
+}
 export function useTranslations(lang: keyof typeof ui) {
   return function t(key: string) {
     return ui[lang][key];
