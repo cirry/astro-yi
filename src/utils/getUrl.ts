2b6d786dfa2c032f2252eda10b678caf2997874c
Cirry Choi <812852553@qq.com>
2025-03-17 18:49:19 +0800

    fix getUrl

--- src/utils/getUrl.ts
+++ src/utils/getUrl.ts
@@ -1,8 +1,9 @@
 const isProd = import.meta.env.PROD;
 // import {}
 export default function getUrl(path: string) {
-  if (path === '' || path === 'javascript:void(0);' || path === '/') {
-    return path
-  }
-  return isProd ? import.meta.env.BASE_URL + path : path
+    if (path === '' || path === 'javascript:void(0);' || path === '/') {
+        return path
+    }
+
+    return (isProd && import.meta.env.BASE_URL !== '/') ? import.meta.env.BASE_URL + path : path
 }
