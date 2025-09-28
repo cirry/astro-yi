661d0b8f4d3ccba1cd09ac1f86516434d0b45432
Cirry Choi <812852553@qq.com>
2025-09-28 14:51:09 +0800

    feat: optimize toc function and remove tocbot package

--- tailwind.config.js
+++ tailwind.config.js
@@ -11,19 +11,19 @@ function withOpacity(variableName) {
 
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-  darkmode: "class",
+  darkmode: 'class',
   theme: {
     screens: {
-      'sm': '600px',
+      sm: '600px',
       // => @media (min-width: 640px) { ... }
 
-      'md': '720px',
+      md: '720px',
       // => @media (min-width: 768px) { ... }
 
-      'lg': '840px',
+      lg: '840px',
       // => @media (min-width: 1024px) { ... }
 
-      'xl': '960px',
+      xl: '960px',
       // => @media (min-width: 1280px) { ... }
 
       '2xl': '1080px',
@@ -34,50 +34,51 @@ export default {
     },
     textColor: {
       skin: {
-        base: withOpacity("--color-text"),
-        dodge: withOpacity("--color-text-dodge"),
-        active: withOpacity("--color-text-active")
+        base: withOpacity('--color-text'),
+        dodge: withOpacity('--color-text-dodge'),
+        active: withOpacity('--color-text-active'),
       },
     },
 
     backgroundColor: {
       skin: {
-        fill: withOpacity("--color-fill"),
-        secondary: withOpacity("--color-fill-secondary"),
-        card: withOpacity("--color-card"),
-        modal: withOpacity("--color-modal"),
+        fill: withOpacity('--color-fill'),
+        secondary: withOpacity('--color-fill-secondary'),
+        card: withOpacity('--color-card'),
+        modal: withOpacity('--color-modal'),
       },
     },
     textDecorationColor: {
       skin: {
-        base: withOpacity("--color-border"),
-        active: withOpacity("--color-text-active")
-      }
+        base: withOpacity('--color-border'),
+        active: withOpacity('--color-text-active'),
+      },
     },
     borderColor: {
       skin: {
         normal: withOpacity('--color-text'),
         base: withOpacity('--color-border'),
+        dodge: withOpacity('--color-text-dodge'),
       },
     },
     extend: {
       colors: {
         custom: {
           title: '#555',
-          subtitle: "#999",
-          hover: "#e0a419",
-          active: "#ff7f50",
-          grey: "#f6f6f6",
-          nav: "#ded6d8",
-          primary: "#edede9",
-          second: "#d6ccc2",
-          third: "#f5ebe0",
-          forth: "#e3d5ca",
-          fifth: "#d5bdaf"
-        }
-      }
+          subtitle: '#999',
+          hover: '#e0a419',
+          active: '#ff7f50',
+          grey: '#f6f6f6',
+          nav: '#ded6d8',
+          primary: '#edede9',
+          second: '#d6ccc2',
+          third: '#f5ebe0',
+          forth: '#e3d5ca',
+          fifth: '#d5bdaf',
+        },
+      },
     },
   },
   plugins: [],
-}
+};
 
